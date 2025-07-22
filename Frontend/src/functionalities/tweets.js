d35ea7e87c50c44539603a7f0ce4b0ae7dbec787
Rudra2637 <singhrudra2637@gmail.com>
2025-07-22 12:20:23 +0530

    Tweet functionalities added

--- Frontend/src/functionalities/tweets.js
+++ Frontend/src/functionalities/tweets.js
@@ -2,12 +2,32 @@ import axios from "axios";
 
 export class TweetService{
     async createTweet(data){
-        const formData = new FormData()
-
         try {
-            
+            const response = await axios.post("/api/v1/tweets/",{
+                content:data
+            })    
+            return response.data
         } catch (error) {
             console.error("Error in creating tweet ",error)
+            throw error
+        }
+    }
+    async getUserTweet(id){
+        try {
+            const response = await axios.get(`/api/v1/tweets/user/${id}`)
+            return response.data
+        } catch (error) {
+            console.error("Error in fetching user Tweets ",error)
+            throw error
+        }
+    }
+    async deleteTweet(id){
+        try {
+            const response = await axios.delete(`/api/v1/tweets/${id}`)
+            return response.data
+        } catch (error) {
+            console.error("Error in deleting user tweets ",error)
+            throw error
         }
     }
 }
