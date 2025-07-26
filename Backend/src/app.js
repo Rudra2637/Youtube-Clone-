5bda707a03b2a1f8cd833e3307578b616f227d21
Rudra2637 <singhrudra2637@gmail.com>
2025-07-26 17:25:55 +0530

    Fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import fileUpload from "express-fileupload"
-
-
-
 
 
 const app = express()
-app.use(fileUpload());
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
